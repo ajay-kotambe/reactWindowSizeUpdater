3d5e60eb55577edc99c341546e5d95f5cbe87b9a
ajay-kotambe <kotambeajay07@gmail.com>
2025-08-05 22:12:38 +0530

    Initial Commit

---
+++ src/components/WindowSizeTracker.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function WindowSizeTracker() {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <div>
+      <p>Width: {windowSize.width}px</p>
+      <p>Height: {windowSize.height}px</p>
+    </div>
+  );
+}
+
+export default WindowSizeTracker;
